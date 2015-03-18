2ab754b9be474f468292b4370270733d1d7457ad
Shane Kumpf <sakserv@sakserv.org>
2015-03-18 13:22:51 -0600

    add port to the tsd init script

--- bin/opentsdb_tcollector_install.sh
+++ bin/opentsdb_tcollector_install.sh
@@ -39,22 +39,20 @@ for node in $TCOLL_HOST_LIST; do
       ssh $SSH_ARGS $node "cd /tmp && git clone https://github.com/OpenTSDB/tcollector.git" || exit 1
    fi
 
-   # Set TSD address in startstop script
-   echo -e "\n#### Setting variables in the tcollector init script"
-   ssh $SSH_ARGS $node "sed -i 's|^.*#.*TSD_HOST=.*|TSD_HOST='$TSD_VIP'|g' /tmp/tcollector/startstop" || exit 1
-   ssh $SSH_ARGS $node "sed -i 's|^.*TCOLLECTOR_PATH=.*|TCOLLECTOR_PATH='$TCOLL_HOME'|g' /tmp/tcollector/startstop" || exit 1
-
    # Move the tcollector to TCOLL_HOME
    echo -e "\n#### Moving tcollector into $TCOLL_HOME"
    ssh $SSH_ARGS $node "mkdir -p $TCOLL_HOME && cp -R /tmp/tcollector/* $TCOLL_HOME/" || exit 1
 
-   # Copy the init script
-   echo -e "\n#### Copying the tcollector init script to /etc/init.d"
-   ssh $SSH_ARGS $node "cp /tmp/tcollector/startstop /etc/init.d/tcollector" || exit 1
+   # Set TSD address in startstop script
+   echo -e "\n#### Setting variables in the tcollector init script"
+   scp $SSH_ARGS /tmp/opentsdb-examples/etc/init.d/tcollector $node:/etc/init.d/
+   ssh $SSH_ARGS $node "sed -i 's|^.*#.*TSD_HOST=.*|TSD_HOST='$TSD_VIP'|g' /etc/init.d/tcollector" || exit 1
+   ssh $SSH_ARGS $node "sed -i 's|^.*#.*TSD_PORT=.*|TSD_PORT='$TSD_PORT'|g' /etc/init.d/tcollector" || exit 1
+   ssh $SSH_ARGS $node "sed -i 's|^.*TCOLLECTOR_PATH=.*|TCOLLECTOR_PATH='$TCOLL_HOME'|g' /etc/init.d/tcollector" || exit 1
 
    # Starting the TSD
    echo -e "\n#### Starting the tcollector on $node"
-   ssh $SSH_ARGS $node "service tcollector start" || exit 1
+   ssh $SSH_ARGS $node "service tcollector restart" || exit 1
 
    echo -e "\n##"
    echo -e "## Successfully completed OpenTSDB TSD install on $node"
