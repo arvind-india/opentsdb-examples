5503f61b154504ecc26a3a673571be6ceb2565f7
Shane Kumpf <sakserv@sakserv.org>
2015-03-20 09:44:05 -0600

    update cli install to use the config

--- bin/opentsdb_cli_install.sh
+++ bin/opentsdb_cli_install.sh
@@ -3,16 +3,7 @@
 #
 # Variables
 #
-source ./opentsdb.conf
-
-#
-# Parse cmdline
-#
-if [ $# -gt 1 ]; then
-   echo "ERROR: Must supply hostname to install on"
-   exit 1
-fi
-node="$1"
+. /tmp/opentsdb-examples/conf/opentsdb.conf
 
 #
 # Sanity Checks
@@ -26,34 +17,36 @@ fi
 # Main
 #
 
-echo -e "\n##"
-echo -e "## Starting OpenTSDB TSD install on $node"
-echo -e "##"
+for node in $TSD_CLI_HOST_LIST; do
+   echo -e "\n##"
+   echo -e "## Starting OpenTSDB TSD CLI wrapper install on $node"
+   echo -e "##"
 
-# Install git, automake, and gnuplot
-echo -e "\n#### Installing git, gnuplot, and automake"
-ssh $SSH_ARGS $node "yum install -y git gnuplot automake" || exit 1
+   # Install git, automake, and gnuplot
+   echo -e "\n#### Installing git, gnuplot, and automake"
+   ssh $SSH_ARGS $node "yum install -y git gnuplot automake" || exit 1
 
-# Build opentsdb
-echo -e "\n#### Cloning OpenTSDB git repo and switching to \"next\" branch"
-if ssh $SSH_ARGS $node "test -d $TSD_HOME/.git"; then
-    echo "WARNING: /tmp/opentsdb already exists... skipping"
-else
-    ssh $SSH_ARGS $node "cd $(dirname $TSD_HOME) && git clone git://github.com/OpenTSDB/opentsdb.git && cd $TSD_HOME && git checkout next" || exit 1
-fi
+   # Build opentsdb
+   echo -e "\n#### Cloning OpenTSDB git repo and switching to \"next\" branch"
+   if ssh $SSH_ARGS $node "test -d $TSD_HOME/.git"; then
+      echo "WARNING: /tmp/opentsdb already exists... skipping"
+   else
+      ssh $SSH_ARGS $node "cd $(dirname $TSD_HOME) && git clone git://github.com/OpenTSDB/opentsdb.git && cd $TSD_HOME && git checkout next" || exit 1
+   fi
 
-# Run the opentsdb build script
-echo -e "\n#### Running the OpenTSDB build script"
-ssh $SSH_ARGS $node "cd $TSD_HOME && PATH=$PATH:$JAVA_BIN ./build.sh" || exit 1
+   # Run the opentsdb build script
+   echo -e "\n#### Running the OpenTSDB build script"
+   ssh $SSH_ARGS $node "cd $TSD_HOME && PATH=$PATH:$JAVA_BIN ./build.sh" || exit 1
 
-# Create TSD log dir
-echo -e "\n#### Creating the TSD log dir $TSD_LOG_DIR"
-ssh $SSH_ARGS $node "mkdir -p $TSD_LOG_DIR" || exit 1
+   # Create TSD log dir
+   echo -e "\n#### Creating the TSD log dir $TSD_LOG_DIR"
+   ssh $SSH_ARGS $node "mkdir -p $TSD_LOG_DIR" || exit 1
 
-# Creating the CLI wrapper
-echo -e "\n#### Creating the CLI wrapper script"
-ssh $SSH_ARGS $node "echo \"cd $TSD_HOME && ./build/tsdb \$\"@\" --zkquorum=$ZK_QUORUM --zkbasedir=$ZK_HBASE_ROOT\" >$TSD_HOME/tsdb.cli && chmod 755 $TSD_HOME/tsdb.cli"  || exit 1
+   # Creating the CLI wrapper
+   echo -e "\n#### Creating the CLI wrapper script"
+   ssh $SSH_ARGS $node "echo \"cd $TSD_HOME && ./build/tsdb \$\"@\" --zkquorum=$ZK_QUORUM --zkbasedir=$ZK_HBASE_ROOT\" >$TSD_HOME/tsdb.cli && chmod 755 $TSD_HOME/tsdb.cli"  || exit 1
 
-echo -e "\n##"
-echo -e "## Successfully completed OpenTSDB TSD install on $node"
-echo -e "##"
+   echo -e "\n##"
+   echo -e "## Successfully completed OpenTSDB TSD CLI install on $node"
+   echo -e "##"
+done
