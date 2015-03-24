ce137a72ed3abb5c2b09280444f4fe56bb5b2c20
Shane Kumpf <sakserv@sakserv.org>
2015-03-24 04:35:13 -0600

    added step to copy sample data to hdfs

--- bin/opentsdb_load_sample_data.sh
+++ bin/opentsdb_load_sample_data.sh
@@ -22,13 +22,25 @@ echo -e "## Starting sample data load from $(hostname)"
 echo -e "##"
 
 # Download the data sets
-echo -e "\n#### Downloading the datasets"
+echo -e "\n#### Downloading and extracting the datasets"
 for dataSet in $CRIMES_DATA $WEATHER_DATA; do
-  echo "Downloading $dataSet"
+  echo "Downloading and extracting $dataSet"
   cd /tmp && wget -N $dataSet
   fileName=$(echo $dataSet | awk -F\/ '{print $NF}')
-  echo "FILE: $fileName"
   cd /tmp && tar -xvzf $fileName
+done
+
+# Copying the data into HDFS
+echo -e "\n#### Loading sample data into HDFS at $SAMPLE_DATA_HDFS_DIR"
+if hdfs dfs -test -d $SAMPLE_DATA_HDFS_DIR; then
+  hdfs dfs -rm -r $SAMPLE_DATA_HDFS_DIR
+fi
+hdfs dfs -mkdir $SAMPLE_DATA_HDFS_DIR
+
+for dataSet in $CRIMES_DATA $WEATHER_DATA; do
+  fileName="$(echo $dataSet | awk -F\/ '{print $NF}' | awk -F\. '{print $1}').csv"
+  echo "Putting /tmp/$fileName into HDFS"
+  hdfs dfs -put /tmp/$fileName $SAMPLE_DATA_HDFS_DIR
 done
 
 exit 0
